0f28a2a546491cf5be97ab777cd2fa2f6bb39348
David <david.14587@gmail.com>
2020-07-08 17:06:50 +1000

    Adding more tests for loop parsing

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,28 +5,29 @@ HelloWorld = "{o}=} {o}=} {={o} {o}=} {={o} {o}=} {={o} {o}=} {={o} {o}=} {={o} 
 
 
 fibDirty = "{o}=} {o}=} push 2 1's
-        {o}==========} {o}==} {=={o} push 10
-        {==={*} peek stack, exit loop if it is 0
-        {o}=} push 1
-        {====={o} put 10 in memory 1
-        {={*} peek top 2 spots and push their sum
-        {o}=} push 1
-        {======{o} pop 1 get memory location 1
-        {*}=} push -1
-        {={o} pop 10 -1 push 10+-1
-        {===={*} exit loop if 10-1!=0
-        {{o} {======={o} pop and print 2nd"
+{o}==========} {o}==} {=={o} push 10
+{==={*} peek stack, exit loop if it is 0
+{o}=} push 1
+{====={o} put 10 in memory 1
+{={*} peek top 2 spots and push their sum
+{o}=} push 1
+{======{o} pop 1 get memory location 1
+{*}=} push -1
+{={o} pop 10 -1 push 10+-1
+{===={*} exit loop if 10-1!=0
+{{o} {======={o} pop and print 2nd"
 
 fibClean = "{o}=} {o}=} {o}==========} {o}==} {=={o} {==={*} {o}=} {====={o}
-        {={*} {o}=} {======{o} {*}=} {={o} {===={*} {{o} {======={o}"
+{={*} {o}=} {======{o} {*}=} {={o} {===={*} {{o} {======={o}"
 
 breakTest = "{o}=} {======={o} {=========={o} {o}=} {======={o}"
 
 skipLoops1 = "{o}} {==={*} {o}=} {===={*} {======={o}"
-
 skipLoops2 = "{o}=} {==={*} {o}} {===={*} {======={o} {======={o}"
-
 skipLoops3 = "{o}} {==={*} {o}=} {===={*} {o}} {======={o} {======={o}"
+
+loopParse1 = "{==={o} {===={o} {===={o} {==={o} {==={o}"
+loopParse2 = "{==={o} {===={o} {==={o} {==={o} {===={o}"
 
 @testset "churrosJulia.jl" begin
     @test churro("{o}====} {======={o} {{==o}")=="4"
@@ -39,4 +40,16 @@ skipLoops3 = "{o}} {==={*} {o}=} {===={*} {o}} {======={o} {======={o}"
     @test churro(skipLoops2)=="01"
     @test churro(skipLoops3)=="00"
     @test churro(fibClean,diagnostics = true)=="55"
+    @test try
+        churro(loopParse1)
+        return "no error"
+    catch e
+        return e.msg
+    end == "Too many loops were closed"
+    @test try
+        churro(loopParse2)
+        return "no error"
+    catch e
+        return e.msg
+    end == "Not enough loops were closed"
 end
